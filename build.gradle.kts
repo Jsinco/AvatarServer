bc5088caafe3a2e7931b333005ae687b81136c2c
Jsinco <jonahmarksinclair@gmail.com>
2024-07-24 00:30:05 -0400

    dump

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+import org.apache.tools.ant.filters.ReplaceTokens
+
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "dev.jsinco.avatarserver"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    implementation("org.eclipse.jetty:jetty-server:9.0.2.v20130417")
+    implementation("org.eclipse.jetty:jetty-io:9.0.2.v20130417")
+    implementation("org.eclipse.jetty:jetty-util:9.0.2.v20130417")
+    implementation("org.eclipse.jetty:jetty-servlet:9.0.2.v20130417")
+    implementation("org.eclipse.jetty:jetty-security:9.0.2.v20130417")
+    implementation("org.eclipse.jetty:jetty-http:9.0.2.v20130417")
+}
+
+tasks {
+
+    java {
+        toolchain.languageVersion = JavaLanguageVersion.of(17)
+    }
+
+    assemble {
+        dependsOn(shadowJar)
+    }
+
+    processResources {
+        outputs.upToDateWhen { false }
+        filter<ReplaceTokens>(mapOf(
+            "tokens" to mapOf("version" to project.version.toString()),
+            "beginToken" to "\${",
+            "endToken" to "}"
+        ))
+    }
+
+    shadowJar {
+        dependencies {
+            //include(dependency("org.eclipse.jetty:jetty-server"))
+            //include(dependency("org.eclipse.jetty:jetty-webapp"))
+        }
+        archiveClassifier.set("")
+    }
+
+    jar {
+        manifest {
+            attributes("Main-Class" to "dev.jsinco.avatarserver.StandaloneServer")
+        }
+        enabled = false
+    }
+
+    withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+    }
+}
---
+++ src/main/java/dev/jsinco/avatarserver/ImageFactory.java
@@ -0,0 +1,86 @@
+package dev.jsinco.avatarserver;
+
+import org.jetbrains.annotations.Nullable;
+
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public final class ImageFactory {
+
+    private static final String TEXTURES_URL = "https://textures.minecraft.net/texture/";
+
+    public static BufferedImage getAvatarImage(String texture) {
+        try {
+            return getAvatarImage(new URL(TEXTURES_URL + texture));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Nullable
+    public static BufferedImage getAvatarImage(URL textureURL) {
+        if (textureURL == null) return null;
+        try {
+            BufferedImage skinImage = ImageIO.read(textureURL);
+
+            int faceStartX = 8, faceStartY = 8;
+            int height = 8, width = 8;
+
+            int overlayStartX = 40;
+            int overlayStartY = 8;
+
+            BufferedImage faceImage = skinImage.getSubimage(faceStartX, faceStartY, width, height);
+            BufferedImage overlayImage = skinImage.getSubimage(overlayStartX, overlayStartY, width, height);
+            return overlayImages(faceImage, overlayImage);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public static BufferedImage overlayImages(BufferedImage background, BufferedImage foreground) {
+        int width = Math.max(background.getWidth(), foreground.getWidth());
+        int height = Math.max(background.getHeight(), foreground.getHeight());
+
+        BufferedImage combinedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB); // ARGB for transparency
+        Graphics2D g = combinedImage.createGraphics();
+
+        // Draw the background image first
+        g.drawImage(background, 0, 0, null);
+
+        // Draw the foreground image on top, specifying coordinates (adjust as needed)
+        g.drawImage(foreground, 10, 10, null); // Adjust x and y for desired placement
+
+        g.dispose(); // Dispose of the graphics context
+        return combinedImage;
+    }
+
+    // Credit: https://stackoverflow.com/questions/7951290/re-sizing-an-image-without-losing-quality
+    public static BufferedImage resizeUsingJavaAlgo(BufferedImage sourceImage, int width, int height) {
+        if (sourceImage == null) {
+            return null;
+        }
+        double ratio = (double) sourceImage.getWidth()/sourceImage.getHeight();
+        if (width < 1) {
+            width = (int) (height * ratio + 0.4);
+        } else if (height < 1) {
+            height = (int) (width /ratio + 0.4);
+        }
+
+        Image scaled = sourceImage.getScaledInstance(width, height, Image.SCALE_AREA_AVERAGING);
+        BufferedImage bufferedScaled = new BufferedImage(scaled.getWidth(null), scaled.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedScaled.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        g2d.drawImage(scaled, 0, 0, width, height, null);
+        g2d.dispose();
+        return bufferedScaled;
+    }
+}
